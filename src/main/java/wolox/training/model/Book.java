f6a1af4af4e475a917ee4e31ac5f0356089bf78c
Sebastian <sebas199765@gmail.com>
2021-03-10 11:25:07 -0500

    feat: create model class the book
    Signed-off-by: Sebastian <sebas199765@gmail.com>

---
+++ src/main/java/wolox/training/model/Book.java
@@ -0,0 +1,55 @@
+package wolox.training.model;
+
+import com.sun.istack.NotNull;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    private String genre;
+
+    @NotNull
+    @Column(nullable = false)
+    private String author;
+
+    @NotNull
+    @Column(nullable = false)
+    private String image;
+
+    @NotNull
+    @Column(nullable = false)
+    private String title;
+
+    @NotNull
+    @Column(nullable = false)
+    private String subtitle;
+
+    @NotNull
+    @Column(nullable = false)
+    private String publisher;
+
+    @NotNull
+    @Column(nullable = false)
+    private String year;
+
+    @NotNull
+    @Column(nullable = false)
+    private String pages;
+
+    @NotNull
+    @Column(nullable = false)
+    private String isbn;
+}
