0a18ed19e7f69fe4ad7ff48681195754c0ed932a
Johamn Sebastian Rincón Agredo <sebastian.rincon@wolox.co>
2021-03-23 11:32:43 -0500

    custom query book and user optional params

--- src/main/java/wolox/training/repository/UserRepository.java
+++ src/main/java/wolox/training/repository/UserRepository.java
@@ -1,15 +1,23 @@
 package wolox.training.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import wolox.training.model.User;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import wolox.training.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(String username);
+  Optional<User> findByUsername(String username);
 
-    List<User> findAllByBirthdateBetweenAndNameContaining(LocalDate startDate, LocalDate endDate, String sequenceCharacters);
+  @Query(value = "select u "
+      + "from User u "
+      + "where ((u.birthdate <= :end and u.birthdate >= :start))"
+      + "and ((lower(u.name) like concat('%',:name,'%')) or :name is null )")
+  List<User> findAllByBirthdateBetweenAndNameContaining(
+      @Param("start") LocalDate startDate,
+      @Param("end") LocalDate endDate,
+      @Param("name") String sequenceCharacters);
 }
